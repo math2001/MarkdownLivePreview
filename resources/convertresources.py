eaa357a65f01f94aa3b05e67da80ad3f6b64728a
Mathieu PATUREL <australie.p@gmail.com>
2019-11-16 08:28:08 +1100

    don't export png's and compilers to the final users
    Though they are still available to the devs, as they are just
    `export-ignore`d in the .gitattributes. I've done a little bit of
    research, and this attribute prevents the files from being archived by
    git. So, I'm assuming that package control fetches archives of the
    packages, and not the actual ones. But I haven't found any documentation
    on packagecontrol.io stating that. I quickly browsed its source code
    on GitHub, and couldn't really find anything. So, I'm going to contact
    @jfcherng, who showed me this trick on FileManager.

--- resources/convertresources.py
+++ resources/convertresources.py
@@ -1,8 +1,5 @@
 """ A small script to convert the images into base64 data """
 
-# FIXME: ignore this script and the original images in .gitignore so that it pushes to
-#        the GitHub repository but not package control
-
 from base64 import b64encode
 
 with open('404.png', 'rb') as png, open('404.base64', 'wb') as base64:
